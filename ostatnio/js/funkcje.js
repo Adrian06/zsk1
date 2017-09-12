8e96587d1ad29417fc5dd1795483f45636eb4015
Adrian06 <wasilewskiadrian28@gmail.com>
2017-09-12 12:14:36 +0200

    lekcja

--- ostatnio/js/funkcje.js
+++ ostatnio/js/funkcje.js
@@ -4,9 +4,26 @@ function witaj(imie){
 function poleProstokata(a,b){
     document.write("Pole prostokąta = " + wynik);
 }
-var a = prompt("Podaj 1 bok");
-var b = prompt("Podaj 2 bok");
-var wynik = a*b;
-var name = "Janusz";
+//var a = prompt("Podaj 1 bok");
+//var b = prompt("Podaj 2 bok");
+//var wynik = a*b;
+//var name = "Janusz";
 //witaj(name);
-poleProstokata(a,b);
+//poleProstokata(a,b);
+
+function poleObjetosc(szerokosc, dlugosc, wysokosc){
+	var pole = szerokosc * dlugosc;
+	var objetosc = pole * wysokosc;
+	var wynik = [pole , objetosc];
+	return wynik;
+}
+var a = prompt("Podaj szerokość");
+var b = prompt("Podaj długość");
+var c = prompt("Podaj wysokość");
+var x = prompt("Podaj x: 0-pole, 1-objętość")
+var poleProstokata = poleObjetosc(a,b,c)[x];
+console.log(poleProstokata);
+
+//Napisać program, który oblicza obwód, pole koła oraz objętość stożka. Dane użytkownik podaje z klawiatury. Wykorzystaj obiekt Math.Użytkownik wybiera w radio co chce obliczyć.
+
+function obliczenia
