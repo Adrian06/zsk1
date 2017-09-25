cda79833d226b23e50591170a9a6ea3f137b41e6
Adrian06 <wasilewskiadrian28@gmail.com>
2017-09-25 22:17:22 +0200

    ostatnio

--- ostatnio/js/egzamin.js
+++ ostatnio/js/egzamin.js
@@ -2,13 +2,17 @@
 
 var elPierwszy = document.getElementById('pierwszy');
  var elDruga = document.getElementById('druga');
+var elPodstawa = document.getElementById('podstawa');
+var elWykladnik = document.getElementById('wykladnik');
 
 var elDodawanie = document.getElementById('dodawanie');
 var elOdejmowanie= document.getElementById('odejmowanie');
 var elMnozenie = document.getElementById('mnozenie');
 var elDzielenie = document.getElementById('dzielenie');
+var elPotegowanie = document.getElementById('potegowanie');
 
 var elWynik = document.getElementById('wynik');
+var elWynikPotegi = document.getElementById('wynikPotegi');
 
 elDodawanie.onclick = function(){
 	if(elPierwszy.value == '' || elDruga.value == ''){
@@ -41,4 +45,24 @@ elDzielenie.onclick = function(){
 		var wynik = parseFloat(elPierwszy.value) / parseFloat(elDruga.value);
 		elWynik.textContent ='Iloraz wynosi: ' + wynik;
 	}
+}
+elMnozenie.onclick = function(){
+	if(elPierwszy.value == '' || elDruga.value == ''){
+		elWynik.textContent = "Proszę uzupełnić obie liczby";
+	}
+	else{
+		var wynik = parseFloat(elPierwszy.value) * parseFloat(elDruga.value);
+		elWynik.textContent ='Iloczyn wynosi: ' + wynik;
+	}
+}
+elPotegowanie.onclick = function(){
+    if(elPodstawa.value == '' || elWykladnik.value == ''){
+        elWynikPotegi.textContent = 'Proszę uzupełnić obie liczby';
+    }
+    else if (elWykladnik.value < 0){
+        elWynikPotegi.textContent = 'Wykładnik potęgi musi być dodatni';
+    }
+    else{
+        elWynikPotegi.textContent = 'Wynik działania wynosi: ' + Math.pow(elPodstawa.value,elWykladnik.value);
+    }
 }
