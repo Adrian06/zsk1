902975551faf1b93c68824f0226c914707f29ea2
Adrian06 <wasilewskiadrian28@gmail.com>
2017-10-13 11:26:03 +0200

    ostatnio

--- ostatnio/jquery/query_tabela.js
+++ ostatnio/jquery/query_tabela.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
+	/*$('tr:even').css('background','#b2aaaa');
+	$('tr:odd').css('background','#f7f0f0');*/
 
+	$('#tabela1 tr:even').css('background','#b20000');
+	$('#tabela1 tr:odd').css('background','#f7f0f0');
 
+	$('#tabela1 tr:first').css('background','#5ad45f');
+	$('#tabela1 tr:last').css('background','#5ad45f');
+	$('#tabela1 tr:eq(3)').css('color','blue');
+	$('#tabela1 td:eq(3)').css('color','white');
+	$('#tabela1 td:contains("Sól")').css('background','blue'); //rozmiar liter ma znaczenie
 
+	$('#widoczny').click(function(){
+		$('#obraz').show(4000)
+	});
+
+	$('#niewidoczny').click(function(){
+		$('#obraz').hide(1000)
+	});
 });
